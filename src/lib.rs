7209ad28c2a0b0b0a8616e31579abe0ab0a17820
Zefira Shannon <earthlingzephyr@gmail.com>
2022-10-01 23:29:06 -0700

    Change block parsing to allow configuring the marker tag

--- src/lib.rs
+++ src/lib.rs
@@ -21,31 +21,40 @@ pub struct ModuleBlock<Definitions> {
     pub definitions: Vec<Definitions>,
 }
 
-pub fn module<'a, F, O, E>(
-    mut definition: F,
-) -> impl FnMut(&'a str) -> IResult<&'a str, ModuleBlock<O>, E>
+impl<Definitions> ModuleBlock<Definitions> {
+    pub fn new(name: String, definitions: Vec<Definitions>) -> Self {
+        Self { name, definitions }
+    }
+}
+
+impl<'a, T> From<(&'a str, Vec<T>)> for ModuleBlock<T> {
+    fn from((name, items): (&'a str, Vec<T>)) -> Self {
+        ModuleBlock::new(String::from(name), items)
+    }
+}
+
+pub fn block<'a, 'b, F, O, OI, E>(
+    block_tag: &'b str,
+    mut item: F,
+) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
 where
-    F: Parser<&'a str, O, E>,
+    'b: 'a,
+    O: From<(&'a str, Vec<OI>)>,
+    F: Parser<&'a str, OI, E>,
     E: ParseError<&'a str>,
 {
     move |input: &'a str| {
-        let (input, _) = tag("module")(input)?;
+        let (input, _) = tag(block_tag)(input)?;
         let (input, _) = space1(input)?;
         let (input, name) = alphanumeric1(input)?;
         let (input, _) = space1(input)?;
-        let (input, definitions) = delimited(
+        let (input, items) = delimited(
             tag("{"),
-            delimited(space1, many0(|input| definition.parse(input)), space1),
+            delimited(space1, many0(|input| item.parse(input)), space1),
             tag("}"),
         )(input)?;
 
-        Ok((
-            input,
-            ModuleBlock {
-                name: name.to_string(),
-                definitions,
-            },
-        ))
+        Ok((input, O::from((name, items))))
     }
 }
 
@@ -56,14 +65,15 @@ mod tests {
     type Result<T> = IResult<&'static str, T, nom::error::Error<&'static str>>;
 
     #[test]
-    fn parse_module() {
+    fn parse_block() {
         let module_text = "module Base { foo }";
         let expected = ModuleBlock {
             name: String::from("Base"),
             definitions: vec!["foo"],
         };
 
-        let module_res: Result<ModuleBlock<&'static str>> = module(tag("foo"))(module_text);
+        let module_res: Result<ModuleBlock<&'static str>> =
+            block("module", tag("foo"))(module_text);
         let (_, actual) = module_res.expect("failed to parse module");
 
         assert_eq!(expected, actual);
